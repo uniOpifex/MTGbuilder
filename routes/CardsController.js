99e25a6752ed5fa1545b81c3b589354cf271d4db
uniOpifex <njhunit@gmail.com>
2017-11-21 10:13:47 -0500

    setup for react router

--- routes/CardsController.js
+++ routes/CardsController.js
@@ -5,3 +5,17 @@ const mtg = require("mtgsdk");
 var Card = require("../models/cardModel.js");
 var User = require("../models/userModel.js");
 var Deck = require("../models/deckModel.js");
+
+
+//Card Index Route
+router.get("/", function(req, res) {
+	User.findById(req.params.userId)
+		.exec(function(err, user) {
+			res.render("cards/index", {
+				menuOne: "Decks",
+				menuTwo: "Cards",
+				menuOnehref: `/${req.params.userId}/decks`,
+				menuTwohref: `/${req.params.userId}/cards`
+			});
+		});
+});
